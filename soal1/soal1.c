a65057013c4a82c97d185aadbeab1378677a87b3
NR <56571192+Rei091200@users.noreply.github.com>
2020-03-21 12:05:59 +0700

    Update soal1.c
    Perbaikan pada source code

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -8,21 +8,59 @@
 #include <syslog.h>
 #include <string.h>
 #include <time.h>
+#include <wait.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
 
-  if (arg[1]>59 || arg[2]>59 || arg[3]>23){
+  int err=0, len = strlen(argv[1]);
 
-    printf("Input argumen salah!");
+  if(len > 2){
+    err++;
+  }
+  else if(strcmp(argv[1],"*") != 0){
+    if(atoi(argv[1]) > 60 || atoi(argv[1]) < 0){
+      err++;
+    }
+  }
+
+  len = strlen(argv[2]);
+  if(len > 2){
+      err++;
+    }
+    else if(strcmp(argv[1],"*") != 0){
+      if(atoi(argv[2]) > 60 || atoi(argv[2]) < 0){
+        err++;
+      }
+    }
+  len=strlen(argv[3]);
+  if(len > 2){
+    err++;
+  }
+  else if(strcmp(argv[1],"*") != 0){
+    if(atoi(argv[3]) > 24 || atoi(argv[3]) < 0){
+      err++;
+    }
+  }
+
+  struct stat st = {0};
+  if(err > 0){
+    printf("ERROR INPUT!\n");
+    exit(EXIT_FAILURE);
+  }
+  if(stat(argv[4],&st) != 0){
+    printf("ERROR PATH!\n");
     exit(EXIT_FAILURE);
   }
 
   pid_t pid, sid;
+
   pid = fork();
 
   if (pid < 0) {
     exit(EXIT_FAILURE);
   }
+
 
   if (pid > 0) {
     exit(EXIT_SUCCESS);
@@ -37,30 +75,144 @@ int main(int argc, char *argv[]) {
 
   if ((chdir("/")) < 0) {
     exit(EXIT_FAILURE);
-  }
+  } 
 
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
+  
+  
+     close(STDIN_FILENO);
+     close(STDOUT_FILENO);
+     close(STDERR_FILENO);
+
 
   while (1) {
- time_t rawtime;
- struct tm* timeinfo;
- time(&rawtime);
- timeinfo=localtime(&rawtime);
+    pid_t run;
+    time_t rawtime;
+    struct tm * timeinfo;
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
 
-struct stat = {0};
-if(err > 0){
- printf("ERROR INPUT\n");
- exit(EXIT_FAILURE);
-}
+    char *program[] = {"bash",argv[4],NULL};
 
-if(stat(argv[4],&st) != 0){
- printf("ERROR PATH\n");
- exit(EXIT_FAILURE);
- } 
-}
+    if(strcmp(argv[1],"*") == 0){
+      if(strcmp(argv[2],"*") == 0){
 
-sleep(1);
+        if(strcmp(argv[3],"*") == 0){
+          while(1){
+            if(run = fork() == 0){
+              execvp("/bin/bash",program);
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime (&rawtime);
+          }
+        }
+
+        else{
+          while (1) {
+            if(atoi(argv[3]) == timeinfo->tm_hour){
+              if(run = fork() == 0){
+                execvp("/bin/bash",program);
+              }
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime (&rawtime);
+          }
+        }
+      }
+
+      else{
+        if(strcmp(argv[3],"*") == 0){
+          while (1) {
+            if(atoi(argv[2]) == timeinfo->tm_min){
+              if(run = fork() == 0){
+                execvp("/bin/bash", program);
+              }
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime ( &rawtime );
+          }
+        }
+
+        else{
+          while (1) {
+            if(atoi(argv[3]) == timeinfo->tm_hour && atoi(argv[2]) == timeinfo->tm_min){
+              if(run = fork() == 0){
+                execvp("/bin/bash", program);
+              }
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime ( &rawtime );
+          }
+        }
+      }
+    }
+
+
+
+    else {
+
+
+      if(strcmp(argv[2],"*") == 0){
+        if(strcmp(argv[3],"*") == 0){
+          while (1) {
+            if(atoi(argv[1]) == timeinfo->tm_sec){
+              if(run = fork() == 0){
+                execvp("/bin/bash", program);
+              }
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime ( &rawtime );
+          }
+        }
+
+        else{
+          while (1) {
+            if(atoi(argv[1]) == timeinfo->tm_sec && atoi(argv[3]) == timeinfo->tm_hour){
+              if(run = fork() == 0){
+                execvp("/bin/bash", program);
+              }
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime ( &rawtime );
+          }
+        }
+      }
+
+
+      else{
+
+        if(strcmp(argv[3],"*") == 0){
+          while(1){
+            if(atoi(argv[2]) == timeinfo->tm_min && atoi(argv[1]) == timeinfo->tm_sec){
+              if(run = fork() == 0){
+                execvp("/bin/bash", program);
+              }
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime ( &rawtime );
+          }
+        }
+
+        else{
+          while (1) {
+            if(atoi(argv[3]) == timeinfo->tm_hour && atoi(argv[2]) == timeinfo->tm_min && atoi(argv[1]) == timeinfo->tm_sec){
+              if(run = fork() == 0){
+                execvp("/bin/bash", program);
+              }
+            }
+            sleep(1);
+            time(&rawtime);
+            timeinfo = localtime ( &rawtime );
+          }
+        }
+      }
+    }
+    break;
   }
 }
